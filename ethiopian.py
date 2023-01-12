29cba11cd18d5ac6df3936dc79b350b69acea35a
kjungw1025 <kjungw1025@naver.com>
2023-01-12 09:32:00 +0900

    feat: Make ethiopian multiplication

--- ethiopian.py
+++ ethiopian.py
@@ -1,5 +1,25 @@
-def hello():
-    print("Good luck!!")
+def div(value):
+    value = value // 2
+    return value
 
-if __name__=='__main__':
-    hello()
+def mul(value):
+    value = value * 2
+    return value
+
+if __name__ == '__main__':
+    n = int(input("숫자를 입력하세요: "))
+    m = int(input("숫자를 입력하세요: "))
+
+    value1 = n
+    value2 = m
+
+    sum = 0
+    while (n > 1):
+        n = div(n)
+        m = mul(m)
+
+        if (n % 2 == 1):
+            sum += m
+
+    print(str(value1) + " x " + str(value2) + " = " + str(sum))
+
